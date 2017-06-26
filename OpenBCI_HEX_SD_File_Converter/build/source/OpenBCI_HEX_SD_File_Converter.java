d18525c210b01f6af4ab105aa9e70db1f24fa35d
biomurph <biomurph@gmail.comr544rre>
2017-06-26 16:38:40 -0400

    Improved the verbose feedback

--- OpenBCI_HEX_SD_File_Converter/build/source/OpenBCI_HEX_SD_File_Converter.java
+++ OpenBCI_HEX_SD_File_Converter/build/source/OpenBCI_HEX_SD_File_Converter.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OpenBCI_HEX_SD_File_Converter extends PApplet {
+
 /**
  * Trying to convert OBCI data from HEX to DECIMAL
  * Started with the Button example, added scratch from other examples.
@@ -15,7 +31,7 @@ float fs_Hz = 250.0f;  //sample rate used by OpenBCI board
 final float ADS1299_Vref = 4.5f;  //reference voltage for ADC in ADS1299
 final float ADS1299_gain = 24;  //assumed gain setting for ADS1299
 final float scale_fac_uVolts_per_count = ADS1299_Vref / (pow(2, 23)-1) / ADS1299_gain  * 1000000.f; //ADS1299 datasheet Table 7, confirmed through experiment
-final float scale_fac_accel_G_per_count = 0.002 / ((float)pow(2,4));  //assume set to +/4G, so 2 mG per digit (datasheet). Account for 4 bits unused
+final float scale_fac_accel_G_per_count = 0.002f / ((float)pow(2,4));  //assume set to +/4G, so 2 mG per digit (datasheet). Account for 4 bits unused
 
 PFont font;
 
@@ -36,13 +52,13 @@ boolean printNextLine = false;
 int buttonX, buttonY;      // Position of square button
 float buttonHeight = 60;     // height of button
 float buttonWidth = 400;
-color buttonColor, buttonHighlight, strokeColor;
-color baseColor;
+int buttonColor, buttonHighlight, strokeColor;
+int baseColor;
 boolean overButton = false;
 boolean reading = false;
 
-void setup() {
-  size(640, 360);
+public void setup() {
+  
   font = createFont("Dialog", 24);
   textFont(font);
   textAlign(CENTER);
@@ -55,7 +71,7 @@ void setup() {
   buttonY = height/2;
 }
 
-void draw() {
+public void draw() {
   background(baseColor);
 
   drawButton();
@@ -102,7 +118,7 @@ void draw() {
 
 
 
-void convert16channelLine() {
+public void convert16channelLine() {
   if(printNextLine){
     for(int i=0; i<hexNums.length; i++){
       h = hexNums[i];
@@ -152,7 +168,7 @@ void convert16channelLine() {
     }
 
     if(i == 0){
-      dataWriter.print(int(floatData[i]));  // print the sample counter
+      dataWriter.print(PApplet.parseInt(floatData[i]));  // print the sample counter
     }else{
       dataWriter.print(floatData[i]);  // print the current channel value
     }
@@ -163,7 +179,7 @@ void convert16channelLine() {
   dataWriter.println();
 }
 
-void convert8channelLine() {
+public void convert8channelLine() {
   if(printNextLine){
     for(int i=0; i<hexNums.length; i++){
       h = hexNums[i];
@@ -213,7 +229,7 @@ void convert8channelLine() {
     }
 
     if(i == 0){
-      dataWriter.print(int(floatData[i]));  // print the sample counter
+      dataWriter.print(PApplet.parseInt(floatData[i]));  // print the sample counter
     }else{
       dataWriter.print(floatData[i]);  // print the current channel value
     }
@@ -222,4 +238,78 @@ void convert8channelLine() {
     }
   }
   dataWriter.println();
+}
+
+
+
+
+public void folderSelected(File selection) {
+  if (selection == null) {
+    println("Window was closed or the user hit cancel.");
+  } else {
+    println("User selected " + selection.getAbsolutePath());
+    dataReader = createReader(selection.getAbsolutePath()); // ("positions.txt");
+    reading = true;
+    println("timing file write");
+    thatTime = millis();
+  }
+}
+
+public void createFile(){
+   logFileName = "OBCI_data/"+month()+"_"+day()+"_"+hour()+"_"+minute()+".csv";
+   dataWriter = createWriter(logFileName);
+   dataWriter.println("%OBCI Data Log " + month()+"/"+day()+" "+hour()+":"+minute());
+}
+
+
+
+
+
+public void drawButton(){
+  updateButton();
+  if (overButton) {
+      fill(buttonHighlight);
+    } else {
+      fill(buttonColor);
+    }
+    stroke(strokeColor);
+    rect(buttonX, buttonY, buttonWidth, buttonHeight);
+    fill(0);
+    text("select file to convert", width/2,height/2 + 10);
+}
+
+
+public void updateButton() {
+  if (buttonOver(buttonX, buttonY, buttonWidth, buttonHeight) ) {
+    overButton = true;
+  } else {
+    overButton = false;
+  }
+}
+
+public boolean buttonOver(int x, int y, float w, float h)  {
+  if (mouseX >= x-w/2 && mouseX <= x+w/2 && 
+      mouseY >= y-h/2 && mouseY <= y+h/2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+public void mousePressed() {
+  if (overButton) {
+    println("mouse pressed");
+    createFile();
+    selectInput("Select a folder to process:", "folderSelected");
+  }
+}
+  public void settings() {  size(640, 360); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OpenBCI_HEX_SD_File_Converter" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
